7bededb9f02a6e04bcf610dc57a42cd111970b46
Irina Borozan <irina@flowx.ai>
2022-12-06 15:40:13 +0200

    receive string message and map to dto in listener

--- src/main/java/ai/flowx/quickstart/connector/kafka/KafkaMessageListener.java
+++ src/main/java/ai/flowx/quickstart/connector/kafka/KafkaMessageListener.java
@@ -3,6 +3,8 @@ package ai.flowx.quickstart.connector.kafka;
 import ai.flowx.commons.trace.aop.Trace;
 import ai.flowx.quickstart.connector.dto.KafkaRequestMessageDTO;
 import ai.flowx.quickstart.connector.service.MessageHandlerService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -16,11 +18,19 @@ public class KafkaMessageListener {
 
     private final MessageHandlerService messageHandlerService;
 
+    private final ObjectMapper objectMapper;
+
     @Trace
     @KafkaListener(topicPattern = "${kafka.topic.in}", containerFactory = "listenerContainerFactory")
-    public void consume(ConsumerRecord<String, KafkaRequestMessageDTO> record) {
+    public void consume(ConsumerRecord<String, String> record) {
         log.info("Received new message from kafka {}.", record.value());
-        KafkaRequestMessageDTO dto = record.value();
+
+        KafkaRequestMessageDTO dto = null;
+        try {
+            dto = objectMapper.readValue(record.value(), KafkaRequestMessageDTO.class);
+        } catch (JsonProcessingException e) {
+            log.error("There was an error when processing received message {}", e.getMessage());
+        }
 
         if (dto == null) {
             log.error("Message invalid: {}", record.value());
